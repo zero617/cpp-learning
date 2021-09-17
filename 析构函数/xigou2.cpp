621172cfe7974c3d96b955a49ca754387dd1811d
zero617 <67105838+zero617@users.noreply.github.com>
2021-09-17 16:12:18 +0800

    Update xigou2.cpp

--- 析构函数/xigou2.cpp
+++ 析构函数/xigou2.cpp
@@ -1,26 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Sample
 {
-    int x,y;
+    int x, y;
+
 public:
-    Sample( ){ x=y=0; }
-    Sample(int a,int b){x=a,y=b;}
-    ~Sample( )
+    Sample() { x = y = 0; }
+    Sample(int a, int b) { x = a, y = b; }
+    ~Sample()
     {
-        if(x==y)
-            cout<<"x=y"<<endl;
+        if (x == y)
+            cout << "x=y" << endl;
         else
-            cout<<"x!=y"<<endl;
+            cout << "x!=y" << endl;
     }
-    void disp( )
+    void disp()
     {
-        cout<<"x="<<x<<",y="<<y<<endl;
+        cout << "x=" << x << ",y=" << y << endl;
     }
 };
 
-int main( )
+int main()
 {
-    Sample(2,3);    //这里定义了对象，但是没有给出对象名，相当于省略了对象名，但是对象的参数给出了
+    Sample(2, 3); //这里定义了对象，但是没有给出对象名，相当于省略了对象名，但是对象的参数给出了
 }
